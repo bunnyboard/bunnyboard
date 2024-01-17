b80077df974e41729286fa8deb8668e6f3cbfcc0
Noat Eden <noat.eth@gmail.com>
2024-01-17 15:53:59 +0700

    add venus snapshot and activity collector

---
+++ src/modules/adapters/venus/abis.ts
@@ -0,0 +1,7 @@
+import { CompoundEventInterfaces, CompoundEventSignatures } from '../compound/abis';
+
+export const VenusEventSignatures: CompoundEventInterfaces = {
+  ...CompoundEventSignatures,
+  DistributedSupplierRewards: '0xfa9d964d891991c113b49e3db1932abd6c67263d387119707aafdd6c4010a3a9',
+  DistributedBorrowerRewards: '0x837bdc11fca9f17ce44167944475225a205279b17e88c791c3b1f66f354668fb',
+};
--- src/modules/adapters/venus/venus.ts
+++ src/modules/adapters/venus/venus.ts
@@ -1,9 +1,12 @@
+import CompoundComptrollerAbi from '../../../configs/abi/compound/Comptroller.json';
+import cErc20Abi from '../../../configs/abi/compound/cErc20.json';
 import VenusComptrollerAbi from '../../../configs/abi/venus/VenusComptoller.json';
 import { CompoundLendingMarketConfig } from '../../../configs/protocols/compound';
 import { formatBigNumberToString } from '../../../lib/utils';
 import { ProtocolConfig } from '../../../types/configs';
 import { ContextServices } from '../../../types/namespaces';
 import CompoundAdapter from '../compound/compound';
+import { VenusEventSignatures } from './abis';
 
 interface Speeds {
   supplySpeed: string;
@@ -15,6 +18,76 @@ export default class VenusAdapter extends CompoundAdapter {
 
   constructor(services: ContextServices, config: ProtocolConfig) {
     super(services, config);
+
+    this.abiConfigs.eventSignatures = VenusEventSignatures;
+    this.abiConfigs.eventAbis = {
+      cErc20: cErc20Abi,
+      comptroller: [
+        ...CompoundComptrollerAbi,
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'contract CToken',
+              name: 'cToken',
+              type: 'address',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'borrower',
+              type: 'address',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'compDelta',
+              type: 'uint256',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'compBorrowIndex',
+              type: 'uint256',
+            },
+          ],
+          name: 'DistributedBorrowerVenus',
+          type: 'event',
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'contract CToken',
+              name: 'cToken',
+              type: 'address',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'supplier',
+              type: 'address',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'compDelta',
+              type: 'uint256',
+            },
+            {
+              indexed: false,
+              internalType: 'uint256',
+              name: 'compSupplyIndex',
+              type: 'uint256',
+            },
+          ],
+          name: 'DistributedSupplierVenus',
+          type: 'event',
+        },
+      ],
+    };
   }
 
   protected async getMarketCompSpeeds(
