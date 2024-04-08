78e65abbcfc9e4ab0e45073f33adaea1052d2f88
Noat Eden <noat.eth@gmail.com>
2024-04-09 04:53:36 +0700

    disable tokenBoard

--- src/configs/boards/tokenBoard.ts
+++ src/configs/boards/tokenBoard.ts
@@ -1,54 +1,53 @@
-import { DataMetrics, TokenBoardConfig } from '../../types/configs';
-import { TokensBookBase } from '../data';
+import { TokenBoardConfig } from '../../types/configs';
 
 export const TokenBoardConfigs: Array<TokenBoardConfig> = [
-  {
-    ...TokensBookBase.ethereum.DAI,
-    protocol: 'maker',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: true,
-    birthday: 1573689600, // Thu Nov 14 2019 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.USDC,
-    protocol: 'circle',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: true,
-    birthday: 1533340800, // Sat Aug 04 2018 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.USDT,
-    protocol: 'tether',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: true,
-    birthday: 1511913600, // Wed Nov 29 2017 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.WBTC,
-    protocol: 'wbtc',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: false,
-    birthday: 1543104000, // Sun Nov 25 2018 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.SUSHI,
-    protocol: 'sushi',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: false,
-    birthday: 1598486400, // Thu Aug 27 2020 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.UNI,
-    protocol: 'uniswap',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: false,
-    birthday: 1600128000, // Tue Sep 15 2020 00:00:00 GMT+0000
-  },
-  {
-    ...TokensBookBase.ethereum.MKR,
-    protocol: 'maker',
-    metric: DataMetrics.tokenBoard,
-    stablecoin: false,
-    birthday: 1511654400, // Sun Nov 26 2017 00:00:00 GMT+0000
-  },
+  // {
+  //   ...TokensBookBase.ethereum.DAI,
+  //   protocol: 'maker',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: true,
+  //   birthday: 1573689600, // Thu Nov 14 2019 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.USDC,
+  //   protocol: 'circle',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: true,
+  //   birthday: 1533340800, // Sat Aug 04 2018 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.USDT,
+  //   protocol: 'tether',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: true,
+  //   birthday: 1511913600, // Wed Nov 29 2017 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.WBTC,
+  //   protocol: 'wbtc',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: false,
+  //   birthday: 1543104000, // Sun Nov 25 2018 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.SUSHI,
+  //   protocol: 'sushi',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: false,
+  //   birthday: 1598486400, // Thu Aug 27 2020 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.UNI,
+  //   protocol: 'uniswap',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: false,
+  //   birthday: 1600128000, // Tue Sep 15 2020 00:00:00 GMT+0000
+  // },
+  // {
+  //   ...TokensBookBase.ethereum.MKR,
+  //   protocol: 'maker',
+  //   metric: DataMetrics.tokenBoard,
+  //   stablecoin: false,
+  //   birthday: 1511654400, // Sun Nov 26 2017 00:00:00 GMT+0000
+  // },
 ];
