f383b731b51735616ac9060f7615ef4125634eea
Noat Eden <noat.eth@gmail.com>
2024-05-22 18:52:04 +0700

    add venus on ethereum

--- src/configs/protocols/venus.ts
+++ src/configs/protocols/venus.ts
@@ -1,5 +1,6 @@
 import { DataMetrics } from '../../types/configs';
 import cTokenMappings from '../data/statics/cTokenMappings.json';
+import { ChainNames, ProtocolNames } from '../names';
 import { CompoundProtocolConfig, formatCompoundLendingMarketConfig } from './compound';
 
 export const VenusConfigs: CompoundProtocolConfig = {
@@ -24,5 +25,127 @@ export const VenusConfigs: CompoundProtocolConfig = {
         '0x20bff4bbeda07536ff00e073bd8359e5d80d733d': true, // CAN
       },
     },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706054400, // Wed Jan 24 2024 00:00:00 GMT+0000
+    //   address: '0x94c1495cd4c557f1560cbd68eab0d197e6291571',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706572800, // Tue Jan 30 2024 00:00:00 GMT+0000
+    //   address: '0x3344417c9360b963ca93a4e8305361aede340ab9',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706572800, // Tue Jan 30 2024 00:00:00 GMT+0000
+    //   address: '0x1b43ea8622e76627b81665b1ecebb4867566b963',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706572800, // Tue Jan 30 2024 00:00:00 GMT+0000
+    //   address: '0xd933909a4a2b7a4638903028f44d1d38ce27c352',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706572800, // Tue Jan 30 2024 00:00:00 GMT+0000
+    //   address: '0x23b4404e4e5ec5ff5a6ffb70b7d14e3fabf237b0',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: 'bnbchain',
+    //   protocol: 'venus',
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1715817600, // Thu May 16 2024 00:00:00 GMT+0000
+    //   address: '0x33b6fa34cd23e5aeed1b112d5988b026b8a5567d',
+    //   governanceToken: {
+    //     chain: 'bnbchain',
+    //     symbol: 'XVS',
+    //     decimals: 18,
+    //     address: '0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63',
+    //   },
+    //   underlying: cTokenMappings,
+    // },
+
+    // ethereum
+    {
+      chain: ChainNames.ethereum,
+      protocol: ProtocolNames.venus,
+      version: 'compound',
+      metric: DataMetrics.crossLending,
+      birthday: 1704844800, // Wed Jan 10 2024 00:00:00 GMT+0000
+      address: '0x687a01ecf6d3907658f7a7c714749fac32336d1b',
+      governanceToken: null,
+      underlying: cTokenMappings,
+    },
+    // {
+    //   chain: ChainNames.ethereum,
+    //   protocol: ProtocolNames.venus,
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1704844800, // Wed Jan 10 2024 00:00:00 GMT+0000
+    //   address: '0x67aa3ecc5831a65a5ba7be76bed3b5dc7db60796',
+    //   governanceToken: null,
+    //   underlying: cTokenMappings,
+    // },
+    // {
+    //   chain: ChainNames.ethereum,
+    //   protocol: ProtocolNames.venus,
+    //   version: 'compound',
+    //   metric: DataMetrics.crossLending,
+    //   birthday: 1706140800, // Thu Jan 25 2024 00:00:00 GMT+0000
+    //   address: '0xf522cd0360ef8c2ff48b648d53ea1717ec0f3ac3',
+    //   governanceToken: null,
+    //   underlying: cTokenMappings,
+    // },
   ]),
 };
--- src/modules/adapters/venus/venus.test.ts
+++ src/modules/adapters/venus/venus.test.ts
@@ -14,7 +14,7 @@ const memcache = new MemcacheService(DefaultMemcacheTime);
 const oracle = new OracleService();
 const blockchain = new BlockchainService();
 
-const timestamp = 1704240000; // Wed Jan 03 2024 00:00:00 GMT+0000
+const timestamp = 1711929600; // Mon Apr 01 2024 00:00:00 GMT+0000
 
 test('should get data correctly at birthday - venus chain bnbchain', async function () {
   const adapter = new VenusAdapter(
@@ -68,6 +68,42 @@ test(`should get data correctly at ${getDateString(timestamp)} - venus chain bnb
 
     if (dataState) {
       expect(dataState.length).equal(30);
+
+      for (const reserve of dataState) {
+        expect(
+          reserve.tokenPrice,
+          `can not get price of ${reserve.token.chain}:${reserve.token.address} at ${timestamp}`,
+        ).not.eq('0');
+      }
+    }
+  }
+});
+
+const currentTimestamp = 1716249600;
+test(`should get data correctly at ${getDateString(currentTimestamp)} - venus chain ethereum`, async function () {
+  const adapter = new VenusAdapter(
+    {
+      blockchain: blockchain,
+      oracle: oracle,
+    },
+    {
+      database: database,
+      memcache: memcache,
+    },
+    VenusConfigs,
+  );
+
+  const configEthereum = ProtocolConfigs.venus.configs.filter((item) => item.chain === 'ethereum')[0];
+  if (configEthereum) {
+    const dataState = await adapter.getLendingReservesDataState({
+      config: configEthereum,
+      timestamp: currentTimestamp,
+    });
+
+    expect(dataState).not.equal(null);
+
+    if (dataState) {
+      expect(dataState.length).equal(9);
 
       for (const reserve of dataState) {
         expect(
