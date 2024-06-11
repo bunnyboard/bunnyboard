20c2ae928b843beb5133d341ae04fd386bef9433
Noat Eden <noat.eth@gmail.com>
2024-06-12 00:49:30 +0700

    limit block scan merlin chain

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -29,6 +29,7 @@ import { ZerolendConfigs } from './protocols/zerolend';
 export const DefaultQueryContractLogsBlockRange = 1000;
 export const CustomQueryContractLogsBlockRange: { [key: string]: number } = {
   polygon: 200,
+  merlin: 200,
 };
 
 export const DefaultMemcacheTime = 300; // 5 minutes
