bf0505647e3096983a693867c343f510ee6da05b
Noat Eden <noat.eth@gmail.com>
2024-07-08 22:15:38 +0700

    fix: fluid birthday

--- src/configs/protocols/fluid.ts
+++ src/configs/protocols/fluid.ts
@@ -19,7 +19,7 @@ export const FluidConfigs: FluidProtocolConfig = {
       protocol: ProtocolNames.fluid,
       metric: DataMetrics.isolatedLending,
       version: LendingMarketVersions.cross.fluid,
-      birthday: 1708992000, // Tue Feb 27 2024 00:00:00 GMT+0000
+      birthday: 1717200000, // Sat Jun 01 2024 00:00:00 GMT+0000
       address: '0x324c5dc1fc42c7a4d43d92df1eba58a54d13bf2d',
       liquidityResolver: '0xd7588f6c99605ab274c211a0afec60947668a8cb',
       vaultResolver: '0x56ddf84b2c94bf3361862fcedb704c382dc4cd32',
