df4303dcf7efdbbaf4374975a7e5e4d0c13c1449
Noat Eden <noat.eth@gmail.com>
2024-07-12 02:40:06 +0700

    add fluid on arbitrum

--- src/configs/protocols/fluid.ts
+++ src/configs/protocols/fluid.ts
@@ -24,5 +24,15 @@ export const FluidConfigs: FluidProtocolConfig = {
       liquidityResolver: '0xd7588f6c99605ab274c211a0afec60947668a8cb',
       vaultResolver: '0x56ddf84b2c94bf3361862fcedb704c382dc4cd32',
     },
+    {
+      chain: ChainNames.arbitrum,
+      protocol: ProtocolNames.fluid,
+      metric: DataMetrics.isolatedLending,
+      version: LendingMarketVersions.cross.fluid,
+      birthday: 1718150400, // Wed Jun 12 2024 00:00:00 GMT+0000
+      address: '0x324c5dc1fc42c7a4d43d92df1eba58a54d13bf2d',
+      liquidityResolver: '0x46859d33e662d4bf18eeed88f74c36256e606e44',
+      vaultResolver: '0x77648d39be25a1422467060e11e5b979463bea3d',
+    },
   ],
 };
